6b659089c89ca32364a7b617926a1e794a33de1d
YoadTew <yoad.tewel@gmail.com>
2021-07-13 17:48:36 +0300

    - Added resnet decoder - Added config for input of original image to view encoder in addition to transformed image experiments

---
+++ models/decoder_resnet.py
@@ -0,0 +1,59 @@
+# ResNet generator and discriminator
+from torch import nn
+import torch.nn.functional as F
+
+from spectral_normalization import SpectralNorm
+import numpy as np
+
+channels = 3
+
+class ResBlockGenerator(nn.Module):
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResBlockGenerator, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, 1, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, 1, padding=1)
+        nn.init.xavier_uniform(self.conv1.weight.data, 1.)
+        nn.init.xavier_uniform(self.conv2.weight.data, 1.)
+
+        self.model = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=2),
+            self.conv1,
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            self.conv2
+            )
+        self.bypass = nn.Sequential()
+        if stride != 1:
+            self.bypass = nn.Upsample(scale_factor=2)
+
+    def forward(self, x):
+        return self.model(x) + self.bypass(x)
+
+
+GEN_SIZE=96
+
+class Generator(nn.Module):
+    def __init__(self, z_dim=128):
+        super(Generator, self).__init__()
+        self.z_dim = z_dim
+
+        self.dense = nn.Linear(self.z_dim, 4 * 4 * GEN_SIZE)
+        self.final = nn.Conv2d(GEN_SIZE, channels, 3, stride=1, padding=1)
+        nn.init.xavier_uniform(self.dense.weight.data, 1.)
+        nn.init.xavier_uniform(self.final.weight.data, 1.)
+
+        self.model = nn.Sequential(
+            ResBlockGenerator(GEN_SIZE, GEN_SIZE, stride=2),
+            ResBlockGenerator(GEN_SIZE, GEN_SIZE, stride=2),
+            ResBlockGenerator(GEN_SIZE, GEN_SIZE, stride=2),
+            nn.BatchNorm2d(GEN_SIZE),
+            nn.ReLU(),
+            self.final,
+            nn.Sigmoid())
+
+    def forward(self, z):
+        return self.model(self.dense(z).view(-1, GEN_SIZE, 4, 4))
